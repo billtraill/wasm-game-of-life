cb0d57bca4b4de7f53425346e8fb915a4413c74a
billtraill <bill@3ideas.co.uk>
2020-03-16 14:37:27 +0000

    pause button added

--- www/index.js
+++ www/index.js
@@ -59,16 +59,43 @@ canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
+let animationId = null;
 
 const renderLoop = () => {
   fps.render();
+  debugger;
   universe.tick();
 
   //drawGrid();
   drawCells();
 
-  requestAnimationFrame(renderLoop);
+  animationId = requestAnimationFrame(renderLoop);
 };
+
+const isPaused = () => {
+    return animationId === null;
+  };
+
+  const playPauseButton = document.getElementById("play-pause");
+
+  const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+  };
+  
+  const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+  };
+  
+  playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+      play();
+    } else {
+      pause();
+    }
+  });
 
 
 const drawGrid = () => {
@@ -122,4 +149,5 @@ const drawGrid = () => {
 
   drawGrid();
   drawCells();
-  requestAnimationFrame(renderLoop);
+  // This used to be `requestAnimationFrame(renderLoop)`.
+play();
